9c676ee36fc3600c2bad7104ed282c180242cd93
lfgetl <bijiare77@gmail.com>
2025-10-13 16:01:23 +0300

    collate fix attempt

--- src/datasets/collate.py
+++ src/datasets/collate.py
@@ -15,34 +15,22 @@ def collate_fn(dataset_items: list[dict]):
             of the tensors.
     """
     result_batch = dict()
-    for k in dataset_items[0].keys():
-        result_batch[k] = []
-    result_batch["spectrogram_length"] = []
-    result_batch["text_encoded_length"] = []
-    for el in dataset_items:
-        for k, v in el.items():
-            if k in result_batch.keys():
-                if k in ["audio", "text", "audio_path"]:
-                    result_batch[k].append(v)
-                else:
-                    dv = v.squeeze(0)
-                    if k == "spectrogram":
-                        dv = dv.permute(1, 0)
-                    result_batch[k + "_length"].append(dv.size(dim=0))
-                    result_batch[k].append(dv)
-
+    for k in ["text", "audio_path"]:
+        result_batch[k] = [elem[k] for elem in dataset_items]
+    result_batch["audio"] = [elem["audio"].squeeze(0) for elem in dataset_items]
     result_batch["spectrogram"] = pad_sequence(
-        result_batch["spectrogram"], batch_first=True
+        [elem["spectrogram"].squeeze(0).permute(1, 0) for elem in dataset_items],
+        batch_first=True,
     )
     result_batch["spectrogram"] = result_batch["spectrogram"].permute(0, 2, 1)
-    result_batch["text_encoded"] = torch.nn.utils.rnn.pad_sequence(
+    result_batch["text_encoded"] = pad_sequence(
         [elem["text_encoded"].squeeze(0) for elem in dataset_items], batch_first=True
     )
     result_batch["spectrogram_length"] = torch.Tensor(
-        result_batch["spectrogram_length"]
+        [elem["spectrogram"].shape[-1] for elem in dataset_items]
     ).long()
     result_batch["text_encoded_length"] = torch.Tensor(
-        result_batch["text_encoded_length"]
+        [elem["text_encoded"].shape[-1] for elem in dataset_items]
     ).long()
 
     return result_batch
